96c9a3b62c03e8293d2aba3576fab2ef9d1a0ce2
Ayush Jain <ayush@Ayushs-MacBook-Pro.local>
2015-08-05 21:07:49 -0500

    Sorting related algorithms added

---
+++ sort_algos.py
@@ -0,0 +1,104 @@
+import random
+
+def selection(arr, idx, lo = 0, hi = None):
+#   Returns the idx-th order element in arr[lo] to arr[hi]
+#   Complexity: O(n) on average
+    if hi == None:
+        hi = len(arr) - 1
+    if idx > hi - lo + 1 or idx <= 0:
+        raise SyntaxError('The order of the element to be found must be <= the length of the array and >= 0')
+    if hi == lo:
+        return lo
+    r = random.randint(lo, hi)
+    arr[lo], arr[r] = arr[r], arr[lo]
+    pivot = arr[lo]
+    i = lo + 1
+    for j in range(lo+1,hi+1):
+        if arr[j]<=pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[lo], arr[i-1] = arr[i-1], arr[lo]
+    if idx == i-lo:
+        return arr[i-1]
+    elif idx > i - lo:
+        return selection(arr, idx - i + lo , i, hi)
+    else:
+        return selection(arr, idx, lo, i-1)
+    
+def quickSort(arr, lo = 0, hi = None):
+#   Sorts the array arr in-place using quicksort
+#   Runtime: O(n*log(n)) on average
+    if hi == None:
+        hi = len(arr) - 1
+    if hi<=lo:
+        return
+    r = random.randint(lo, hi)
+    arr[lo], arr[r] = arr[r], arr[lo]
+    pivot = arr[lo]
+    i = lo + 1
+    for j in range(lo+1,hi+1):
+        if arr[j]<=pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[lo], arr[i-1] = arr[i-1], arr[lo]
+    quickSort(arr, lo, i-2)
+    quickSort(arr, i, hi)
+
+def mergeSort(arr, start = 0, end = None, sort = 'ascending'):
+#   Sorts the array arr using mergesort - arr is modified
+#   Returns the number of inversions in arr
+#   Runtime: O(n*log(n))
+    if end == None:
+        end = len(arr)-1
+    if start == end:
+        return 0
+    mid = (start+end)/2
+    inversions = mergeSort(arr,start,mid)
+    inversions += mergeSort(arr, mid + 1, end)
+    inversions += merge(arr, start, mid, end)
+    
+    return inversions
+
+def merge(arr, start, mid, end, sort = 'ascending'):
+    a = [0 for i in range(start,end+1)]
+    p1 = start
+    p2 = mid+1
+    inversions = 0
+    k = 0
+    while(p1<=mid and p2<=end):
+        if arr[p1]<=arr[p2]:
+            a[k] = arr[p1]
+            k += 1
+            p1 += 1
+        else:
+            inversions += mid - p1 + 1
+            a[k] = arr[p2]
+            p2 += 1
+            k += 1
+    while (p1<=mid):
+        a[k] = arr[p1]
+        k += 1
+        p1 += 1
+    while (p2<=end):
+        a[k] = arr[p2]
+        p2 += 1
+        k += 1
+    
+    if sort == 'descending':
+        a.reverse()
+    arr[start:end+1] = a
+    return inversions
+
+
+'''    
+import random
+arr = [random.randint(0,10000000) for i in range(1000000)]
+quickSort(arr)
+
+for i in range(len(arr)-1):
+    assert arr[i+1]>=arr[i]
+print 'Sorted an array of size 1000000'
+
+arr = [100-i for i in range(100)]
+print selection(arr, 20)
+'''
