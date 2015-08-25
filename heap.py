a0c1955e9c2f3943f35fcaad4c4bddefd123f2e8
Ayush Jain <ayush@wirelessprvnat-172-17-141-213.near.illinois.edu>
2015-08-25 15:44:16 -0500

    Updating stream median Algorithm

--- heap.py
+++ heap.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 #   Defines the heap class
 #   The class creates a min-heap based on key
 #   Also provides functionality to search items by value
@@ -8,10 +10,13 @@ class heap:
     def __init__(self, arr = [], key = lambda x:x[0], value = lambda x:x[1]):
     # Creates a heap out of list arr using key and value functions
     # If no argument is provided, creates an empty heap
-        self.heapArr = arr
+        self.heapArr = deepcopy(arr)
         self.valtoIndex = {}
         self.func_val = value
         self.func_key = key
+        
+    def len(self):
+        return len(self.heapArr)
     
     def isEmpty(self):
     # Checks if heap is empty
@@ -22,9 +27,9 @@ class heap:
         self.heapArr.append(element)
         self.valtoIndex[self.func_val(element)] = len(self.heapArr)-1
         self.bubbleUp(len(self.heapArr)-1)
+        #print self.heapArr, self.valtoIndex
         
     def bubbleUp(self, ind):
-        
         parent = (ind - 1)/2
         child = ind
         while(child!=0 and self.func_key(self.heapArr[child])<self.func_key(self.heapArr[parent])):
@@ -38,7 +43,6 @@ class heap:
         
     def removeElement(self,value):
         # Removes and returns the element with specific value
-        assert len(self.heapArr) == len(self.valtoIndex)
         idx = self.valtoIndex[value]
         lastElement = self.heapArr.pop()
         del self.valtoIndex[value]
@@ -51,18 +55,16 @@ class heap:
         
         self.bubbleUp(idx)
         self.bubbleDown(idx)
-        assert len(self.heapArr) == len(self.valtoIndex)
         return element
         
     def bubbleDown(self, ind):
-        
         _length = len(self.heapArr)
         curr = ind
         child = 2*curr + 1
         if (_length>2*curr+2):
             if self.func_key(self.heapArr[2*curr+1])>self.func_key(self.heapArr[2*curr+2]):
                 child = 2*curr + 2
-        while(child<len(self.heapArr)-1 and self.func_key(self.heapArr[child])<self.func_key(self.heapArr[curr])):
+        while(child<len(self.heapArr) and self.func_key(self.heapArr[child])<self.func_key(self.heapArr[curr])):
             self.heapArr[curr], self.heapArr[child] = self.heapArr[child],self.heapArr[curr]
             self.valtoIndex[self.func_val(self.heapArr[child])] = child
             self.valtoIndex[self.func_val(self.heapArr[curr])] = curr
@@ -73,10 +75,11 @@ class heap:
             else:
                 child = 2*curr + 1
             
+            
+            
                 
     def pop(self):
         #   Removes and returns the minimum key element in the heap
-        assert len(self.heapArr) == len(self.valtoIndex)
         minElement = self.heapArr[0]
         del self.valtoIndex[self.func_val(minElement)]
         lastElement = self.heapArr.pop()
@@ -84,24 +87,71 @@ class heap:
             self.heapArr[0] = lastElement
             #print self.heapArr
             self.valtoIndex[self.func_val(lastElement)] = 0
-        
             self.bubbleDown(0)
         
-        assert len(self.heapArr) == len(self.valtoIndex)
         return minElement
     
-   
         
+
+        
+        
+class medianMaintenance:
+    def __init__(self, KEY = lambda x:x[0], VAL = lambda x:x[1]):
+        self.minheap = heap(key = lambda x:KEY(x), value = VAL)
+        self.maxheap = heap(key = lambda x:-KEY(x), value = VAL)
+        self.func_key = KEY
+        
+        
+    def addElement(self, element):
+        
+        if (self.minheap.len()>0) and self.maxheap.len()>0:
+            more = self.minheap.pop()
+            less = self.maxheap.pop()
+            self.minheap.addElement(more)
+            self.maxheap.addElement(less)
+            if self.func_key(element)<self.func_key(less):
+                self.maxheap.addElement(element)
+            else:
+                self.minheap.addElement(element)
+            
+        else:
+            self.minheap.addElement(element)
+        if self.maxheap.len()>(self.minheap.len()+1):
+            self.minheap.addElement(self.maxheap.pop())
+        elif self.minheap.len()>(self.maxheap.len()+1):
+            self.maxheap.addElement(self.minheap.pop())
+        assert abs(self.minheap.len() - self.maxheap.len())<=1
+                
+        
+    def popMedian(self):
+        if (self.minheap.len() + self.maxheap.len())%2 == 0:
+            return self.maxheap.pop()
+        elif self.minheap.len() > self.maxheap.len():
+            return self.minheap.pop()
+        else:
+            return self.maxheap.pop()
+        
+            
 if __name__ == '__main__':
-    value = lambda x:x[3]
-    pq = heap(key = lambda x:x[2], value = lambda x:x[3])
+    '''
+    A = medianMaintenance(KEY = lambda x:x, VAL = lambda x:x)
+    for a in range(25):
+        print a,
+        A.addElement(a)
+        A.sprint()
+        m = A.popMedian()
+        A.addElement(m)
+        print m
+    
+    
+    exit
+    '''
+    value = lambda x:x
+    pq = heap(key = lambda x:x, value = lambda x:x)
     for i in range(10):
-        pq.addElement([1,2,10-i,10-i])
+        pq.addElement(10-i)
         print 'Inserted element', 10-i
         
     for i in range(10):
-        print 'Minimum element', value(pq.pop()), 'deleted' 
-        
-        
-            
+        print 'Minimum element', value(pq.pop()), 'deleted'
         
